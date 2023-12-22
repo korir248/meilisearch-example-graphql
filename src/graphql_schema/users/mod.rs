9fbd1096bcf50edf391dd2d3253fef414ecba757
Eugene Korir <korir16071@gmail.com>
2023-12-23 00:11:29 +0300

    Added mutation for creating a user

--- src/graphql_schema/users/mod.rs
+++ src/graphql_schema/users/mod.rs
@@ -1,9 +1,2 @@
-use async_graphql::MergedObject;
-pub mod get_users;
-pub mod search;
-
-use get_users::GetUsers;
-use search::SearchUsers;
-
-#[derive(Default, MergedObject)]
-pub struct UsersQuery(pub GetUsers, pub SearchUsers);
+pub mod mutation;
+pub mod query;
